d35a6e3329d110937277ec7b9086f748a7c39d87
taylorconde <taylorconde@gmail.com>
2025-09-24 19:27:20 -0300

    Corrige lógica de saque em WithdrawalGenerateTransactionServiceImpl

--- src/tech/pinho/banco/service/WithdrawalGenerateTransactionServiceImpl.java
+++ src/tech/pinho/banco/service/WithdrawalGenerateTransactionServiceImpl.java
@@ -1,4 +1,14 @@
 package tech.pinho.banco.service;
 
-public class WithdrawalGenerateTransactionServiceImpl {
+import java.math.BigDecimal;
+
+public class WithdrawalGenerateTransactionServiceImpl implements GenerateTransactionService {
+
+
+    public WithdrawalGenerateTransactionServiceImpl()
+
+    @Override
+    public void execute(int accountNumber, BigDecimal amount, String description) {
+
+    }
 }
